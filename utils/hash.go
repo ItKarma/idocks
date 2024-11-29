e5e005e554269ff6509723989e27cbede3b4999e
ItKarma <loppesofc@gmail.com>
2024-11-29 17:42:57 -0300

    add: add hash for password

---
+++ utils/hash.go
@@ -0,0 +1,15 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword gera um hash a partir de uma senha
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashed), err
+}
+
+// VerifyPassword verifica se uma senha corresponde ao hash
+func VerifyPassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
