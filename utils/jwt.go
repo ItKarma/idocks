3d12d7df4080f55188098173c42deffbf8798912
ItKarma <loppesofc@gmail.com>
2024-11-29 17:43:52 -0300

    add: add util jwt generate

---
+++ utils/jwt.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// secret key for jwt
+var jwtKey = []byte("MySecretKey")
+
+// cria um token jwt para o usuario
+func GerenateToken(userID string) (string, error) {
+	claims := jwt.MapClaims{
+		"userID": userID,
+		"exp":    time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+// verifica se o token jwt é valido
+func ValidateToken(tokenStr string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, err
+	}
+}
